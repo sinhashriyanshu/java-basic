9ffa272f1536d570e1ad984b9377ef3bc1a5e8f7
Shriyanshu Sinha <shriyanshusinhapatna@gmail.com>
2024-08-03 19:21:42 +0530

    Create ternery.java

---
+++ ternery.java
@@ -0,0 +1,14 @@
+// ternary operator 
+class ternary
+{
+public static void main(String args[]) 
+{ 
+   
+    int n1 = 5, n2 = 10, max; 
+   
+    max = (n1 > n2) ? n1 : n2; 
+     
+    System.out.println("Largest number " + n1 + 
+                  " and " + n2 + " is " + max + ". " ); 
+} 
+}
